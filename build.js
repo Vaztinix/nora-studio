34a95b0c776860a23a9499e14e8e03a61114d349
Damien Adams <vaztinixstudios@gmail.com>
2026-05-27 11:33:39 -0400

    build: flatten dist folder structure to fix GitHub Pages 404

--- build.js
+++ build.js
@@ -4,11 +4,10 @@ const path = require('path');
 const root = __dirname;
 const srcWeb = path.join(root, 'src', 'web');
 const distDir = path.join(root, 'dist');
-const distWeb = path.join(distDir, 'web');
 
 fs.rmSync(distDir, { recursive: true, force: true });
-fs.mkdirSync(distWeb, { recursive: true });
+fs.mkdirSync(distDir, { recursive: true });
 
-fs.cpSync(srcWeb, distWeb, { recursive: true });
+fs.cpSync(srcWeb, distDir, { recursive: true });
 
-console.log(`Build complete: ${distWeb}`);
+console.log(`Build complete: ${distDir}`);
